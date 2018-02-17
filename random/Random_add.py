cb200e6191e368612386a65b9a682befbc122cd2
Milos Matic <milos.matic1513@gmail.com>
2018-02-17 16:46:03 +0200

    alages arxiwn

--- random/Random_add.py
+++
@@ -1,9 +0,0 @@
-import random
-def rand_table(lines,rows):
-	table=[]
-	alph=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-	for i in range (lines):
-		table.append([])
-		for j in range(rows):
-			table[i].append(random.choice(alph))
-	return table
