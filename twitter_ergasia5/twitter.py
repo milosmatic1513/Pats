81cfa999b77b4fe2328aa8803d4bbd9768c7bb13
milosmatic1513 <milos.matic1513@gmail.com>
2018-02-18 16:55:49 +0200

    twitter fixes

--- twitter_ergasia5/twitter.py
+++ twitter_ergasia5/twitter.py
@@ -5,15 +5,17 @@ while True:
 	
 	while True:
 		try:
-			user=raw_input("Please enter user : ")
+			user=raw_input("Please enter user (type 'exit' to exit): ")
 			timeline = twitter_add.get_timeline(user)
-
+			if( user=="exit"):
+				break
 			table=[]
 			for line in timeline:
 				table.append(line.text)
 			new_table=[]
 			temp=[]
 			for line in table:
+				
 				temp=line.split(" ")
 				for word in temp:
 					new_table.append(word)
@@ -32,7 +34,25 @@ while True:
 			for i in range(len(word_times)):
 				if(word_times[i]==max_word):
 					print "-"+just_words[i]
+			while True:
+				ans=raw_input("would you like to see the tweets?(y/n):")
+				if (ans=="y"):
+					for i in range(10):
+						temp=table[i].split(" ")
+						print i+1,")",;
+						for word in temp:
+							try:
+								print word ,
+							except UnicodeError:
+								pass
+						print "\n"
+					break
+				elif (ans=="n"):
+					break
 			break
 		except:
-			print "user not found"
+			if (user=="exit"):
+				break
+			else:
+				print "user not found"
 	break
